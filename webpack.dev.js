6bb2b0f532406511fc8300e960f023db13086757
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-07 17:55:26 -0300

    chore: Add specialized configs

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html'
+    })
+  ]
+}
