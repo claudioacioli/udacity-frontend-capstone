2c846703f2d082f7cef3797135d31735e7cbcd52
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-11 11:21:16 -0300

    style: Remove logs

--- src/client/js/travel.js
+++ src/client/js/travel.js
@@ -13,11 +13,7 @@ const
 export const  
   getTravelInfo = async city => {
     
-    console.log(city);
     const result = await getInfoByCity(city)
-    console.log(result);
-
-    console.log(wallElement);
 
     wallElement.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 40%), url(${result.images[1].url})`;
     placeImgElement.src = result.images[0].url;
@@ -25,6 +21,8 @@ export const
     weatherElement.textContent = result.weather.description;
     weatherImgElement.src = `https://www.weatherbit.io/static/img/icons/${result.weather.icon}.png`
     tempElement.textContent = result.temp;
+
+    
   }
 ;
 
