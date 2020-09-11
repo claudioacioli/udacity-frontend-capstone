5f44134cf1b48a78bb30c4ba9c2a4b64566514b4
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-11 11:20:38 -0300

    chore: Correct the production mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: './src/client/index.js',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main.js'
@@ -17,11 +28,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ['style-loader','css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'cssura-[name].css'
+    }),
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
