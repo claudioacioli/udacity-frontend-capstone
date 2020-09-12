88783c0e72035c52010fb248cef29c0b75093ea0
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-12 20:44:22 -0300

    refactor: Use constants and change export

--- src/client/js/modal.js
+++ src/client/js/modal.js
@@ -1,6 +1,10 @@
 import { bySelector, byId } from './by';
+import { renderResetForm } from './form';
 
 const 
+
+  CSS_CLASS_SHOW = 'modal--show',
+
   dropElement = bySelector('.modal__backdrop'),
   modalElement = bySelector('.modal'),
   closeElement = byId('close'),
@@ -8,22 +12,25 @@ const
   showModal = () => {
     closeElement.addEventListener('click', hideModal);
     dropElement.addEventListener('click', hideModal);
-    modalElement.classList.add('modal--show');
+    modalElement.classList.add(CSS_CLASS_SHOW);
   },
 
   hideModal = () => {
     closeElement.removeEventListener('click', hideModal);
     dropElement.removeEventListener('click', hideModal);
-    modalElement.classList.remove('modal--show');
-  }
-;
+    modalElement.classList.remove(CSS_CLASS_SHOW);
+  },
 
-export const
-  handleOpenModal = e =>
+  handleOpenModal = e => 
     showModal()
   ,
 
-  handleCloseModal = e =>
-    hideModal()
+  handleCloseModal = e => 
+    hideModal();
 ;
+
+export {
+  handleOpenModal,
+  handleCloseModal
+}
 
