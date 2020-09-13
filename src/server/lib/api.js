c5d88f82acafb8635cc8a81285f4e1556563a07b
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-13 11:42:13 -0300

    feat: Create key/value for icon

--- src/server/lib/api.js
+++ src/server/lib/api.js
@@ -23,12 +23,17 @@ const
     const url = new URL(`https://api.weatherbit.io/v2.0${path}&key=${key}`)
     const result = await getData(url)
     const data = result && "data" in result && result.data ? result.data : []
-    return data[0]
+    return data.length ? data[0] : [];
   },
 
   getWeatherByCoords = async (lat, lng) => {
     const url = `/current?lat=${lat}&lon=${lng}`
-    return await getDataFromWeatherBit(url)
+    const result = await getDataFromWeatherBit(url)
+    return { 
+      weather_icon_url: `https://www.weatherbit.io/static/img/icons/${result.weather.icon}.png`,
+      ...result
+    }
+
   },
 
   getDataFromPixabay = async q => {
