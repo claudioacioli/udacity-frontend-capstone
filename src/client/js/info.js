bac8391a3cc4b3ee219ea93d67065d9ef7eb63f0
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-13 11:42:39 -0300

    feat: Use key from api result

--- src/client/js/info.js
+++ src/client/js/info.js
@@ -9,12 +9,12 @@ const
   weatherElement = byId('weather'),
   weatherImgElement = byId('weather-img'), 
 
-  render = async ({temp, weather, city_name, images}) => {
+  render = async ({temp, weather_icon_url, weather, city_name, images}) => {
     wallElement.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 40%), url(${images[1].url})`;
     placeImgElement.src = images[0].url;
     nameElement.textContent = city_name;
     weatherElement.textContent = weather.description;
-    weatherImgElement.src = `https://www.weatherbit.io/static/img/icons/${weather.icon}.png`
+    weatherImgElement.src = weather_icon_url;
     tempElement.textContent = temp;
   }
 ;
