e5dba6925a65ea583d67606fc4691544d6552ab1
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-13 10:05:42 -0300

    refactor: Import api route

--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
-// Utilities
-const { getCoordsByCity, getWeatherByCoords, getImagesByCity } = require('./lib/api')
+// Routes
+const {rootRoute} = require('./routes')
 // Settings
 const port = 3000
 const app = express()
@@ -14,25 +14,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-app.get('/api/', async (req, res) => {
-  
-  const city = req.query.city ? req.query.city.toString().trim() : ''
-  const date = req.query.date ? req.query.date.toString().trim() : ''
-
-  const geoResult = city.length
-    ? await getCoordsByCity(city)
-    : {}
-  
-  const { lat=0, lng=0 } = geoResult;
-  const weatherResult = lat && lng 
-    ? await getWeatherByCoords(lat, lng)
-    : {}
-
-  const images = await getImagesByCity(city)
-
-  res.send({...geoResult, ...weatherResult, images})
-
-})
+app.get('/api/', rootRoute)
 
 //Routes
 app.use((req, res) => {
