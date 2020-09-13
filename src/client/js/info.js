d18b153695e2113260256b673591ba70c6f8de29
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-13 11:22:17 -0300

    refact: Use spreed

--- src/client/js/info.js
+++ src/client/js/info.js
@@ -2,20 +2,20 @@ import { byId, bySelector } from './by';
 
 const
 
-  wallElement = bySelector(".bg--city"),
+  wallElement = bySelector('.bg--city'),
   placeImgElement = byId('place-img'),
   nameElement = byId('name'),
   tempElement = byId('temp'),
   weatherElement = byId('weather'),
   weatherImgElement = byId('weather-img'), 
 
-  render = async result => {
-    wallElement.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 40%), url(${result.images[1].url})`;
-    placeImgElement.src = result.images[0].url;
-    nameElement.textContent = result.city_name;
-    weatherElement.textContent = result.weather.description;
-    weatherImgElement.src = `https://www.weatherbit.io/static/img/icons/${result.weather.icon}.png`
-    tempElement.textContent = result.temp;
+  render = async ({temp, weather, city_name, images}) => {
+    wallElement.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 40%), url(${images[1].url})`;
+    placeImgElement.src = images[0].url;
+    nameElement.textContent = city_name;
+    weatherElement.textContent = weather.description;
+    weatherImgElement.src = `https://www.weatherbit.io/static/img/icons/${weather.icon}.png`
+    tempElement.textContent = temp;
   }
 ;
 
