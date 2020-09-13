559d5c2b246a080a00c97cdfb71cd1d56f7c42ab
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-13 10:28:31 -0300

    feat: Create file of constants

---
+++ src/client/js/constants.js
@@ -0,0 +1,9 @@
+export const
+  // CSS Classes
+  CSS_CLASS_INPUT_ERROR = 'textfield--error',
+  CSS_CLASS_MODAL_SHOW = 'modal--show',
+  // String messages
+  MESSAGE_ERROR_EMPTY_CITY = 'Please specify the city of your next travel!',
+  MESSAGE_ERROR_EMPTY_DATE = 'Please specify the date of your next travel!',
+  MESSAGE_ERROR_INVALID_DATE = 'Please specify a valid date: Is tomorrow more then today.'
+;
