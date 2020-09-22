1202f6517b276be00555c15f205154a2b1d13aa9
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-21 22:06:29 -0300

    feat: Create constants

--- src/client/js/constants.js
+++ src/client/js/constants.js
@@ -2,6 +2,7 @@ export const
   // CSS Classes
   CSS_CLASS_INPUT_ERROR = 'textfield--error',
   CSS_CLASS_MODAL_SHOW = 'modal--show',
+  CSS_CLASS_HIDE = 'hide',
   // String messages
   MESSAGE_ERROR_EMPTY_CITY = 'Please specify the city of your next travel!',
   MESSAGE_ERROR_EMPTY_DATE = 'Please specify the date of your next travel!',
