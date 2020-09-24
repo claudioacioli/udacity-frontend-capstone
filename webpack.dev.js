30a4afd460424ba20628959d92df22c124350046
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-23 23:15:31 -0300

    feat: Configure plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -27,7 +28,16 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    }),
+    new HtmlWebpackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
     })
