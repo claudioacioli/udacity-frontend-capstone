ab7a8d1e447fc875476074e73086fbca6dd6950c
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-26 16:37:12 -0300

    feat: Use historical api call

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,23 @@
-const { getCoordsByCity, getWeatherByCoords, getImagesByCity } = require('../lib/api')
+const { getCoordsByCity, getHistoricalWeatherByCoords, getImagesByCity } = require('../lib/api')
 
 const 
+
   apiRoute = async (req, res) => {
   
     const city = "city" in req.query ? req.query.city.toString().trim() : ''
-    const date = "date" in req.query ? req.query.date.toString().trim() : ''
+    const start = "start" in req.query ? req.query.start.toString().trim() : ''
+    const end = "end" in req.query ? req.query.end.toString().trim() : ''
+
+    const startDate = new Date(Number(start))
+    const endDate = new Date(Number(end))
 
     const geoResult = city.length
       ? await getCoordsByCity(city)
       : {}
-  
+ 
     const { lat=0, lng=0 } = geoResult;
-    const weatherResult = lat && lng 
-      ? await getWeatherByCoords(lat, lng)
+    const weatherResult = lat && lng && startDate
+      ? await getHistoricalWeatherByCoords(lat, lng, startDate)
       : {}
 
     const images = await getImagesByCity(city)
