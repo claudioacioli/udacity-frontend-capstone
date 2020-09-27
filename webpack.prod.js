3c37b04e54053394bf824af90de9d1e2ee881abb
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-27 13:03:10 -0300

    chore: Install service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,10 @@
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -43,6 +44,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
-    })
+    }),
+    new WorkboxPlugin.GenerateSW()
   ]
 }
