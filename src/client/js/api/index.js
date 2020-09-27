da012c8d0da130b5f1b595d5cf145e291112fdf1
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-27 10:12:59 -0300

    feat: Create method to delete travel on server

--- src/client/js/api/index.js
+++ src/client/js/api/index.js
@@ -1,13 +1,16 @@
-import { getData, postData } from './rest';
+import { getData, postData, deleteData } from './rest';
 import { URL_API } from '../utils/constants';
 
 export const
 
   readTravels = async () =>
-    await getData(URL_API)
+    await getData(`${URL_API}/`)
   ,
 
   createTravel = async (city, start, end) => 
-    await postData(URL_API, {city, start, end})
+    await postData(`${URL_API}/`, {city, start, end})
+  ,
 
+  deleteTravel = async id => 
+    await deleteData(`${URL_API}/${id}`)
 ;
