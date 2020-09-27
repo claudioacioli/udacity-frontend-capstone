33ae0a20b1f88104db08056d7bbce3e5cdc20bc2
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-09-27 08:29:12 -0300

    feat: Initilialize with saved list

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import './styles/cssura-v1.1.5.css'
 import './styles/index.css'
+import { readTravels } from './js/api';
 import Modal from './js/components/modal';
 import Form from './js/components/form';
 import CardList from './js/components/cardList';
@@ -7,27 +8,7 @@ import CardList from './js/components/cardList';
 document.addEventListener('DOMContentLoaded', () => {
   Form.init();
   Modal.init();
-  const list = [
-    {
-      start: '2020-10-05',
-      end: '2020-10-05',
-      city: 'Aracaju',
-      weather: 'Cloudy',
-      temp: 28,
-      images: [
-        {url:'http://www.localizahertz.com/brasil-site/pt-br/rede-de-agencias/PublishingImages/Aracaju/Aracaju-Cabecalho-1.jpg'},
-      ]
-    },
-    {
-      start: '2020-10-05',
-      end: '2020-10-05',
-      city: 'Maceio',
-      weather: 'Cloudy',
-      temp: 22,
-      images: [
-        {url:'http://www.localizahertz.com/brasil-site/pt-br/rede-de-agencias/PublishingImages/Aracaju/Aracaju-Cabecalho-1.jpg'},
-      ]
-    }
-  ];
-  CardList.init(list);
+  readTravels()
+    .then(CardList.init)
+    .catch(err => console.error(err))
 });
